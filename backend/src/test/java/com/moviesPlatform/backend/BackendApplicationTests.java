95292b0affdf9070a8cf42bcdf775b903a5d382e
mahmoud.abolfadl <mahmoud.abolfadl@asset.com.eg>
2025-11-14 02:28:52 +0200

    Add backend Spring boot and frontend Angular 7 initial projects.

---
+++ backend/src/test/java/com/moviesPlatform/backend/BackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.moviesPlatform.backend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BackendApplicationTests {
+
+    @Test
+    public void contextLoads() {
+        // Simple context load test for Spring Boot 2.1 + JUnit 4
+    }
+}
