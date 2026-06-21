9fa973ad74d82b570415606bfa69eb6d9ad4b3f5
mahmoud.abolfadl <mahmoud.abolfadl@asset.com.eg>
2026-06-21 21:32:55 +0300

    fix (cors-config): fixed wrong value parsing of `ANGULAR_APP_URL` -> root cause of login failure.

--- backend/src/main/java/com/moviesPlatform/backend/config/CorsConfig.java
+++ backend/src/main/java/com/moviesPlatform/backend/config/CorsConfig.java
@@ -4,19 +4,20 @@ package com.moviesPlatform.backend.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /*Cross-Origin Resource Sharing*/
 @Configuration
 public class CorsConfig {
-    @Value("{$ANGULAR_APP_URL}")
+    @Value("${ANGULAR_APP_URL}")
     private String angularAppUrl;
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
-            public void addCorsMappings(CorsRegistry registry){
+            public void addCorsMappings(@NonNull CorsRegistry registry){
                 registry.addMapping("/api/**")
                         .allowedOrigins(angularAppUrl) // Angular app origin (To enable cross-origin requests from Angular frontend)
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
